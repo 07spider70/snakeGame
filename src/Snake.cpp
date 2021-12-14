20934ce4f344f5906cfd1ea860b0912818043b48
Ján Remeň <07spider70@gmail.com>
2021-12-14 12:24:40 +0000

    snake

---
+++ src/Snake.cpp
@@ -0,0 +1,69 @@
+#include "Snake.h"
+
+
+Snake::Snake(COORD position, int speed) {
+    position = position;
+    speed = speed
+    dir = LEFT;
+    lenght = 1;
+
+    body.push_back(position);
+}
+
+
+void Snake::setDirection(direction dir) {
+    direction = dir;
+}
+
+void Snake::growUp() {
+    length++;
+}
+COORD Snake::getPosition() {
+    return position;
+}
+
+vector<COORD> Snake::getBody() {
+    return body;
+}
+
+void Snake::moveSnake() {
+    switch (direction) {
+        case UP:
+            position.y -= speed;
+            break;
+        case DOWN:
+            position.y += speed;
+            break;
+        case LEFT:
+            position.x -= speed;
+            break;
+        case RIGHT:
+            position.x += speed;
+            break;
+    }
+
+    body.push_back(position);
+    if(body.size() > length) {
+        body.erase(body.begin());
+    }
+}
+        
+bool Snake::collided() {
+    if(position.x < 1 || position.x > DISPLAY_WIDTH - 2 || position.y < 1 || position.y > DISPLAY_HEIGHT-2) {
+        return true;
+    }
+
+    for(int i = 0; i< lenght-1; i++) {
+        if(position.x == body[i].x && position.y == body[i].y) {
+            return true;
+        }
+    }
+    return false;
+}
+bool Snake::eatApple(COORD apple) {
+    if(position.x == apple.x && position.y == apple.y) {
+        return true;
+    }
+
+    return false;
+}
--- src/Snake.h
+++ src/Snake.h
@@ -8,6 +8,27 @@
 
 class Snake {
 
+    public:
+        Snake(COORD position, int speed);
+
+        void setDirection(direction dir);
+        void growUp();
+        COORD getPosition();
+        vector<COORD> getBody();
+        void moveSnake();
+        
+        bool collided();
+        bool eatApple(COORD apple);
+    
+    private:
+        COORD position;
+        int speed;
+        direction dir;
+        vector<COORD> body;
+        int length;
+
+
+
 };
 
 #endif
